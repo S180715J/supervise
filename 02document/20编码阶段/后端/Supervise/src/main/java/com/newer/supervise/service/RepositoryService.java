ca496326409ebe04694b58699dc2c11fa4915ffa
motaicheng <799212410@qq.com>
2019-03-18 14:23:35 +0800

    Update RepositoryService.java

--- 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/service/RepositoryService.java
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/service/RepositoryService.java
@@ -14,6 +14,12 @@ import com.newer.supervise.pojo.Repository;
 import com.newer.supervise.pojo.SecrecyLevel;
 import com.newer.supervise.pojo.Source;
 
+/**
+ * 备用库的服务层
+ * 
+ * @author W419
+ *
+ */
 @Service
 public class RepositoryService {
 
@@ -25,10 +31,10 @@ public class RepositoryService {
 
 	@Autowired
 	private SecrecyLevelMapper levelMapper;
-	
+
 	@Autowired
 	private RepositoryMapper repositoryMapper;
-	
+
 	/**
 	 * 获取所有的事项来源
 	 * 
@@ -46,30 +52,31 @@ public class RepositoryService {
 	public List<FileType> getAllTypes() {
 		return fileTypeMapper.queryAllTypes();
 	}
-	
+
 	/**
 	 * 获取所有的保密等级
 	 * 
 	 * @return
 	 */
-	public List<SecrecyLevel> getAllLevel(){
+	public List<SecrecyLevel> getAllLevel() {
 		return levelMapper.queryAllLevel();
 	}
-	
+
 	/**
 	 * 获取所有备用库事项（分页）
 	 * 
 	 * @return
 	 */
-	public List<Repository> getAllItem(Integer page, Integer limit){
+	public List<Repository> getAllItem(Integer page, Integer limit) {
 		return repositoryMapper.queryAllItem((page - 1) * limit, limit);
 	}
-	
+
 	/**
 	 * 查询所有的备用库数据
+	 * 
 	 * @return
 	 */
-	public List<Repository>  queryAll(){
-	   return repositoryMapper.queryAll();
+	public List<Repository> queryAll() {
+		return repositoryMapper.queryAll();
 	}
 }
