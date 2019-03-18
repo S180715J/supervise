a9888713d5aacf331f1831cb8438dca201e6f1f9
motaicheng <799212410@qq.com>
2019-03-18 14:40:27 +0800

    Update UserMapper.java

--- 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/UserMapper.java
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/UserMapper.java
@@ -11,12 +11,8 @@ import com.newer.supervise.pojo.User;
 public interface UserMapper {
 
 	@Select("SELECT user_id,user_name,`password`,real_name,d.`duty_id`,duty_name,duty_type FROM `user` u LEFT JOIN duty d ON u.`duty_id`=d.`duty_id` WHERE d.`duty_type`='公司领导'")
-	@Results({@Result(column="user_id",property="userId"),
-			  @Result(column="user_name",property="userName"),
-			  @Result(column="password",property="password"),
-			  @Result(column="real_name",property="realName"),
-			  @Result(column="duty_id",property="duty.dutyId"),
-			  @Result(column="duty_name",property="duty.dutyName"),
-			  @Result(column="duty_type",property="duty.dutyType"),})
+	@Results({ @Result(column = "duty_id", property = "duty.dutyId"),
+			@Result(column = "duty_name", property = "duty.dutyName"),
+			@Result(column = "duty_type", property = "duty.dutyType"), })
 	List<User> queryLeaders();
 }
