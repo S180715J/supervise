cfa3db7de7c628ad09dc61933d350fe56340354f
motaicheng <799212410@qq.com>
2019-03-20 11:06:18 +0800

    Update RepositoryMapper.java

--- 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/RepositoryMapper.java
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/RepositoryMapper.java
@@ -2,6 +2,7 @@ package com.newer.supervise.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
@@ -22,11 +23,11 @@ public interface RepositoryMapper {
 	List<Repository> queryAllItem(@Param("page") Integer page, @Param("limit") Integer limit);
 
 	/**
-	 * 显示备用库所存的所有数据,以备用库的事项编号为级联关系，关联Iten类（事项关系表）的itemStatus，optTime(事项状态，操作时间)属性
+	 * 显示备用库所存的所有数据,以备用库的事项编号为级联关系
 	 * 
 	 * @return
 	 */
-	@Select("SELECT b.source_type,a.item_content,a.source_time,a.adverse_company,c.type_name,a.item_type,a.item_statu,d.opt_time   FROM  repository   a\r\n"
+	@Select("SELECT  id, b.source_type,a.item_content,a.source_time,a.adverse_company,c.type_name,a.item_type,a.item_statu,d.opt_time   FROM  repository   a\r\n"
 			+ "LEFT  JOIN  source  b  ON  a.source_id=b.source_id  \r\n"
 			+ "LEFT  JOIN  file_type c  ON  a.file_type=c.type_id\r\n"
 			+ "LEFT  JOIN  item_process d ON  a.item_code=d.item_code")
@@ -34,5 +35,30 @@ public interface RepositoryMapper {
 			@Result(column = "type_name", property = "fileType.typeName"),
 			@Result(column = "opt_time", property = "itemCode.optTime") })
 	public List<Repository> queryAll();
+	
+	
+	/**
+	 * 回显单个数据
+	 * 
+	 * @return
+	 */
+	@Select("SELECT  id, b.source_type,a.item_content,a.source_time,a.adverse_company,c.type_name,a.item_type,a.item_statu,d.opt_time   FROM  repository   a\r\n"
+			+ "LEFT  JOIN  source  b  ON  a.source_id=b.source_id  \r\n"
+			+ "LEFT  JOIN  file_type c  ON  a.file_type=c.type_id\r\n"
+			+ "LEFT  JOIN  item_process d ON  a.item_code=d.item_code WHERE id=#{id} ")
+	@Results({ @Result(column = "source_type", property = "sourceId.sourceType"),
+			@Result(column = "type_name", property = "fileType.typeName"),
+			@Result(column = "opt_time", property = "itemCode.optTime") })
+	public Repository queryOne(Integer id);
+
+	/**
+	 * 添加新数据
+	 * 
+	 * @param rep
+	 * @return
+	 */
+	@Insert("INSERT INTO repository (source_id,source_time,serial_num,file_type,drafter,drafter_phone,item_name,item_code,user_id,adverse_company,secrecy_level,item_content,over_time,feedback,next_feedback,dept_opinion,lead_opinion,item_statu,item_type,remark)  \r\n"
+			+ "VALUES(#{sourceId.sourceId},#{sourceTime},#{serialNum},#{fileType.typeId},#{drafter},#{drafterPhone},#{itemName},#{itemCode.itemCode},#{user.userId},#{adverseCompany},#{secrecyLevel.levelId},#{itemContent},#{overTime},#{feedback},#{nextFeedback},#{deptOpinion},#{leadOpinion},#{itemStatu},#{itemType},#{remark})")
+	public Integer insert(Repository rep);
 
 }
