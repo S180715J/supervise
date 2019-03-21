882bf3ffbb647f1bb5b463839af0ac358f83c788
motaicheng <799212410@qq.com>
2019-03-21 09:44:18 +0800

    Update ItemProcessMapper.java

--- 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/ItemProcessMapper.java
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/ItemProcessMapper.java
@@ -1,6 +1,8 @@
 package com.newer.supervise.mapper;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 import com.newer.supervise.pojo.ItemProcess;
 
@@ -18,4 +20,13 @@ public interface ItemProcessMapper {
 	     */
 	    @Insert("INSERT INTO item_process (item_code,statu_describe,opt_time,user_id) VALUES(#{itemCode},#{statuDescribe},#{optTime},#{userId.userId})")
 	    public Integer insert(ItemProcess item);
+	    
+	    /**
+	     * 修改事项进程表中事项编号
+	     * @param itemCode
+	     * @param id
+	     * @return
+	     */
+	    @Update("UPDATE item_process SET  item_code=#{itemCode}   WHERE statu_id=#{id}")
+	    public Integer update(@Param("itemCode")String itemCode,@Param("id")Integer id);
 }
