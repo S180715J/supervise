6a9b766adc48877b2c4762591ffa04962924ea68
bugcreatorzhou <44260910+bugcreatorzhou@users.noreply.github.com>
2019-03-23 16:38:11 +0800

    1

--- 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/SuperviseApplication.java
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/SuperviseApplication.java
@@ -4,10 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
+@EnableTransactionManagement
 @MapperScan("com.newer.supervise.mapper")
-
 public class SuperviseApplication {
 
 	public static void main(String[] args) {
--- 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/service/OrgService.java
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/service/OrgService.java
@@ -1,56 +1,71 @@
 package com.newer.supervise.service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.newer.supervise.mapper.OrgMapper;
 import com.newer.supervise.pojo.Organization;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000)
 public class OrgService {
+
+	private List<Integer> list = new LinkedList<>();
+	
 	@Autowired
 	private OrgMapper orgmapper;
 
 	/**
-	 * 查询部门所以信息
+	 * 分页查询当前页面的部门信息
 	 * 
 	 * @param limit 查询条数
 	 * @param page  当前页码
 	 * @return
 	 */
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000, readOnly = true)
 	public List<Organization> getorg(Integer page, Integer limit) {
 		return orgmapper.getorg((page - 1) * limit, limit);
 	}
 
 	/**
-	 * 按orgid查询所有信息
+	 * 按orgid查询对应部门的所有信息
 	 * 
 	 * @param orgid
 	 * @return
 	 */
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000, readOnly = true)
 	public Organization getorgID(Integer orgid) {
 		return orgmapper.getorgID(orgid);
 	}
 
 	/**
-	 * 新增部门信息
+	 * 新增部门
 	 * 
 	 * @param org
 	 * @return
 	 */
-	public int insertorg(Organization org) {
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000, readOnly = false)
+	public Integer insertorg(Organization org) {
 		return orgmapper.insertorg(org);
 	}
 
 	/**
-	 * 删除部门信息
+	 * 根据id删除部门信息
 	 * 
 	 * @param orgid
 	 * @return
 	 */
-	public int delorg(Integer orgid) {
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000, readOnly = false)
+	public Integer delorg(Integer orgid) {
+		Organization organization = orgmapper.getorgID(orgid);
+		Integer id = organization.getOrgId();
+		
 		return orgmapper.delorg(orgid);
 	}
 
@@ -60,7 +75,8 @@ public class OrgService {
 	 * @param org
 	 * @return
 	 */
-	public int uporg(Organization org) {
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000, readOnly = false)
+	public Integer uporg(Organization org) {
 		return orgmapper.uporg(org);
 	}
 
@@ -69,6 +85,7 @@ public class OrgService {
 	 * 
 	 * @return
 	 */
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000, readOnly = true)
 	public Integer getCount() {
 		return orgmapper.queryCountOfDept();
 	}
@@ -78,6 +95,7 @@ public class OrgService {
 	 * 
 	 * @return
 	 */
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, timeout = 5000, readOnly = true)
 	public List<Organization> queryParentPath() {
 		return orgmapper.queryParentPath();
 	}
