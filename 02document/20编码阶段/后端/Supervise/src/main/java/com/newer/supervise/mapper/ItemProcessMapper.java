dfa769de21b6e7a9eb92c43166e39f59f8d1957d
motaicheng <799212410@qq.com>
2019-03-26 10:52:39 +0800

    备用库功能修改完善版，主页service和controller以创建完成

--- 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/ItemProcessMapper.java
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/mapper/ItemProcessMapper.java
@@ -1,7 +1,11 @@
 package com.newer.supervise.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
@@ -9,41 +13,54 @@ import com.newer.supervise.pojo.ItemProcess;
 
 /**
  * 事项进程dao层
+ * 
  * @author W419
  *
  */
 public interface ItemProcessMapper {
-         
-	    /**
-	     * 调用此方法，保存事项编号，操作人，操作时间
-	     * @param item
-	     * @return
-	     */
-	    @Insert("INSERT INTO item_process (item_code,statu_describe,opt_time,user_id,sup_statu,org_statu,staff_statu) VALUES(#{itemCode},#{statuDescribe},#{optTime},#{userId.userId},#{supStatu},#{orgStatu},#{staffStatu})")
-	    public Integer insert(ItemProcess item);
-	    
-	    /**
-	     * 修改事项进程表中所有事项编号
-	     * @param itemCode
-	     * @param id
-	     * @return
-	     */
-	    @Update("UPDATE item_process SET  item_code=#{itemCode}   WHERE item_code=#{oldCode}")
-	    public Integer update(@Param("itemCode")String itemCode,@Param("oldCode")String oldCode);
-	    
-	    /**
-	     * 查找得到最后的时间
-	     * @param item
-	     * @return
-	     */
-	    @Select("SELECT item_code,MAX(opt_time)  FROM  item_process  WHERE item_code=#{itemCode}")
-	    public ItemProcess  selectTime(String itemCode);
-	    
-	    
-	    /**
-	     * 更改事项进程表操作数据
-	     * @return
-	     */
-	    @Update("UPDATE  item_process  SET  statu_describe=#{statuDescribe} ,opt_time=#{optTime}, sup_statu=#{supStatu},org_statu=#{orgStatu},staff_statu=#{staffStatu} WHERE  item_code=#{itemCode} AND user_id=#{userId.userId}  ")
-	    public Integer  updateStatu(ItemProcess item);
+
+	/**
+	 * 调用此方法，保存事项编号，操作人，操作时间
+	 * 
+	 * @param item
+	 * @return
+	 */
+	@Insert("INSERT INTO item_process (item_code,statu_describe,opt_time,user_id,sup_statu,org_statu,staff_statu) VALUES(#{itemCode},#{statuDescribe},#{optTime},#{userId.userId},#{supStatu},#{orgStatu},#{staffStatu})")
+	public Integer insert(ItemProcess item);
+
+	/**
+	 * 修改事项进程表中所有事项编号
+	 * 
+	 * @param itemCode
+	 * @param id
+	 * @return
+	 */
+	@Update("UPDATE item_process SET  item_code=#{itemCode}   WHERE item_code=#{oldCode}")
+	public Integer update(@Param("itemCode") String itemCode, @Param("oldCode") String oldCode);
+
+	/**
+	 * 查找得到最后的时间
+	 * 
+	 * @param item
+	 * @return
+	 */
+	@Select("SELECT item_code,MAX(opt_time)  FROM  item_process  WHERE item_code=#{itemCode}")
+	public ItemProcess selectTime(String itemCode);
+
+	/**
+	 * 更改事项进程表操作数据
+	 * 
+	 * @return
+	 */
+	@Update("UPDATE  item_process  SET  statu_describe=#{statuDescribe} ,opt_time=#{optTime}, sup_statu=#{supStatu},org_statu=#{orgStatu},staff_statu=#{staffStatu} WHERE  item_code=#{itemCode} AND user_id=#{userId.userId}  ")
+	public Integer updateStatu(ItemProcess item);
+
+	/**
+	 * 得到事项进程表中同一事项的具体数据 以id的升序排序，方便在页面操作
+	 * 
+	 * @return
+	 */
+	@Select("SELECT i.statu_id,u.real_name  FROM  item_process i  LEFT JOIN  `user` u ON  i.user_id =u.user_id  WHERE i.item_code=#{itemCode}  ORDER BY  i.statu_id  ASC")
+	@Results({ @Result(column = "real_name", property = "userId.realName") })
+	public List<ItemProcess> queryUser(String itemCode);
 }
---
+++ 02document/20编码阶段/后端/Supervise/src/main/java/com/newer/supervise/service/MainService.java
@@ -0,0 +1,32 @@
+package com.newer.supervise.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.newer.supervise.mapper.ItemProcessMapper;
+import com.newer.supervise.pojo.ItemProcess;
+
+/**
+ * 对首页数据操作的服务层
+ * 
+ * @author W419
+ *
+ */
+@Service
+public class MainService {
+
+	@Autowired
+	private ItemProcessMapper itemMapper;
+
+	/**
+	 * 得到事项进程表中同一事项的具体数据
+	 * 
+	 * @param itemCode
+	 * @return
+	 */
+	public List<ItemProcess> queryUser(String itemCode) {
+		return itemMapper.queryUser(itemCode);
+	}
+}
